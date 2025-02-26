029521f9f36fa917c77452532334c3dd624c8af9
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-02-25 21:22:34 -0300

    add border radius no container

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 10,
-            children: [
-              Container(width: 100, height: 100, color: Colors.black),
-              Container(width: 100, height: 100, color: Colors.red),
-              Container(width: 100, height: 100, color: Colors.blue),
-              Container(width: 100, height: 100, color: Colors.green),
-              Container(width: 100, height: 100, color: Colors.yellow),
-            ],
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(150)),
+            ),
           ),
         ),
       ),
